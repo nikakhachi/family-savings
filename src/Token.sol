66694025ddb4e3633095b50ed9eeeb359ee59344
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2023-07-08 14:05:51 +0400

    comments to Token contract

--- src/Token.sol
+++ src/Token.sol
@@ -3,7 +3,18 @@ pragma solidity 0.8.20;
 
 import "openzeppelin/token/ERC20/ERC20.sol";
 
+/**
+ * @title Token Contract
+ * @author Nika Khachiashvili
+ * @dev A basic ERC20 token contract.
+ */
 contract Token is ERC20 {
+    /**
+     * @dev Contract constructor.
+     * @param _name The name of the token.
+     * @param _symbol The symbol of the token.
+     * @param _initialSupply The initial supply of tokens.
+     */
     constructor(
         string memory _name,
         string memory _symbol,
