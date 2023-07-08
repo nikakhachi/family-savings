337ebe77cf1a555afec60f1ba07150c1b9e07f4c
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2023-07-08 14:08:08 +0400

    update TimeLock contract comments

--- src/TimeLock.sol
+++ src/TimeLock.sol
@@ -3,16 +3,23 @@ pragma solidity 0.8.20;
 
 import "openzeppelin/governance/TimelockController.sol";
 
+/**
+ * @title TimeLock Contract
+ * @author Nika Khachiashvili
+ * @dev The contract important for the Governance. This will be the contract making the direct
+ * @dev calls to the FamilySavings contract on behalf of the proposals
+ */
 contract TimeLock is TimelockController {
-    // minDelay is how long you have to wait before executing
-    // proposers is the list of addresses that can propose
-    // executors is the list of addresses that can execute
-    //`admin`: optional account to be granted admin role; disable with zero address  /**
     /**
-     * IMPORTANT: The optional admin can aid with initial configuration of roles after deployment
-     * without being subject to delay, but this role should be subsequently renounced in favor of
-     * administration through timelocked proposals. Previous versions of this contract would assign
-     * this admin to the deployer automatically and should be renounced as well.
+     * @dev Contract constructor.
+     * @dev IMPORTANT: The optional admin can aid with initial configuration of roles after deployment
+     * @dev without being subject to delay, but this role should be subsequently renounced in favor of
+     * @dev administration through timelocked proposals. Previous versions of this contract would assign
+     * @dev this admin to the deployer automatically and should be renounced as well.
+     * @param minDelay is how long you have to wait before executing
+     * @param proposers is the list of addresses that can propose
+     * @param executors is the list of addresses that can execute
+     * @param admin optional account to be granted admin role; disable with zero address
      */
     constructor(
         uint256 minDelay,
